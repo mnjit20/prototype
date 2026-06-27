45ff623e571d3a160c67ad1f84b63f58ae702bb6
Manjeet Verma <mnjit.vrma@gmail.com>
2026-06-28 00:37:40 +0200

    Add initial implementation for airline check-in system
    - Create package.json and tsconfig.json for project setup
    - Implement database connection in db.ts
    - Develop booking service in booking.service.ts to handle seat booking logic
    - Add index.ts to simulate multiple user booking requests and display booked seats
    - Create reset.ts to reset seat bookings
    - Implement seed.ts to populate the database with initial seat data
    - Structure project into approach1 and approach2 directories for different implementations

---
+++ airline-checkin/approach2/src/booking.service.ts
@@ -0,0 +1,75 @@
+import { pool } from "./db";
+
+const sleep = (ms: number) =>
+    new Promise(resolve => setTimeout(resolve, ms));
+
+export async function bookSeat(
+    user: string,
+    seatNo: string
+) {
+    console.log('Request:: ', user);
+    const client = await pool.connect();
+
+    try {
+
+        await client.query("BEGIN");
+
+        const result = await client.query(
+            `
+            SELECT *
+            FROM seats
+            WHERE seat_no=$1
+            FOR UPDATE
+            `,
+            [seatNo]
+        );
+
+        if (result.rows.length === 0) {
+
+            console.log(`${seatNo} doesn't exist`);
+
+            await client.query("ROLLBACK");
+            return;
+
+        }
+
+        const seat = result.rows[0];
+
+        if (seat.booked) {
+
+            console.log(`${user} failed. ${seatNo} already booked`);
+
+            await client.query("ROLLBACK");
+            return;
+
+        }
+
+        await sleep(Math.random() * 500);
+
+        await client.query(
+            `
+            UPDATE seats
+            SET booked=true,
+                booked_by=$1,
+                booked_at=NOW()
+            WHERE seat_no=$2
+            `,
+            [user, seatNo]
+        );
+
+        await client.query("COMMIT");
+
+        console.log(`✅ ${user} booked ${seatNo}`);
+
+    } catch (err) {
+
+        await client.query("ROLLBACK");
+        console.error(err);
+
+    } finally {
+
+        client.release();
+
+    }
+
+}
---
+++ airline-checkin/approach2/src/seed.ts
@@ -0,0 +1,39 @@
+import { pool } from "./db";
+
+const letters = ["A", "B", "C", "D", "E", "F"];
+
+async function seed() {
+
+    await pool.query("DELETE FROM seats");
+
+    let count = 0;
+
+    for (let row = 1; row <= 100; row++) {
+
+        for (const letter of letters) {
+
+            if (count >= 100)
+                break;
+
+            await pool.query(
+                `
+                INSERT INTO seats(seat_no)
+                VALUES($1)
+                `,
+                [`${row}${letter}`]
+            );
+
+            count++;
+        }
+
+        if (count >= 100)
+            break;
+    }
+
+    console.log(`Inserted ${count} seats`);
+
+    await pool.end();
+
+}
+
+seed();
